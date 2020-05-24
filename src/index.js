1dbf31398786228661ea30be8807961c4ee8ff61
Shubham Sanjay Jain <itshubhamjain@gmail.com>
2020-05-24 10:38:12 -0400

    Starting with components
    Starting with components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
+//Import the React and ReactDOM Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+
+//Create a react components
+
+const App = () => {
+    const buttonText = {text:'Click Me'}
+    const  style = {backgroundColor:'blue', color:"white"};
+    return (
+        <div>
+        <label className="label" htmlFor="name">
+        Enter name:
+        </label>
+        <input id="name" type="text"/>
+        <button style={style}>
+        {buttonText.text}
+        </button>
+        </div>
+    );
+};
+
+// Take the react components and show it on screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
